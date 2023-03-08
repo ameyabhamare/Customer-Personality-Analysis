8a82e1abb256e65b98c282778ef77f9f33fb80cf
ameyabhamare <ameyarb1804@gmail.com>
2023-03-07 16:16:57 -0800

    modularized final visualization script and it runs now

--- heart_rate_analysis/heart_rate_analysis.py
+++ heart_rate_analysis/heart_rate_analysis.py
@@ -1,24 +1,10 @@
-'''This module reads in the daily_values.csv files and displays relvant visualizations'''
+'''
+This module reads in the daily_values.csv files and displays relvant visualizations
+'''
 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-'''
-daily_values = pd.read_csv('C:/AMEYA/UW/Academics/DATA 515/Project/FitMe/\
-                           heart_rate_analysis/data/daily_values.csv')
-'''
-
-
-
-
-
-
-
-sns.boxplot(x = 'Sleep Duration', y = 'Value', data = daily_values, color = 'blue')
-plt.xticks(rotation = 45, ha = 'right')
-plt.show()
-
 
 def plot_daily_heart_rate(daily_values, user_id = None):
     if user_id is None:
@@ -28,11 +14,10 @@ def plot_daily_heart_rate(daily_values, user_id = None):
     ax1 = sns.lineplot(x = 'date_time', y = 'Value', data = daily_values, palette = 'bright')
     ax1.set(xlabel = 'Date')
     ax1.set(ylabel = 'bpm')
-    ax1.set_xticklabels(labels = daily_values['date_time'], size = 6, rotation = 30)
     ax1.plot()
     plt.show()
     
-def plot_weekly_heart_rate(df, user_id = None):
+def plot_weekly_heart_rate(daily_values, user_id = None):
     if user_id is None:
         user_id = '2026352035'
     daily_values = daily_values.query(f"Id == {user_id}")
@@ -42,7 +27,7 @@ def plot_weekly_heart_rate(df, user_id = None):
     ax1.plot()
     plt.show()
 
-def plot_bpm_density(df, user_id = None):
+def plot_bpm_density(daily_values, user_id = None):
     if user_id is None:
         user_id = '2026352035'
     daily_values = daily_values.query(f"Id == {user_id}")
@@ -53,10 +38,15 @@ def plot_bpm_density(df, user_id = None):
     ax1.plot()
     plt.show()
     
-
-def plot_sleep_vs_bpm(df, user_id = None):
-
-
+def plot_sleep_vs_bpm(daily_values, user_id = None):
+    if user_id is None:
+        user_id = '2026352035'
+    daily_values = daily_values.query(f"Id == {user_id}")
+    _, ax1 = plt.subplots(figsize = (12, 6))
+    ax1 = sns.boxplot(x = 'Sleep Duration', y = 'Value', data = daily_values, color = 'blue')
+    ax1.set(xlabel = 'Sleep duration in minutes')
+    ax1.set(ylabel = 'BPM')
+    plt.show()
     
 def create_final_df(heartrate_seconds, daily_sleep):
     heartrate_seconds['date_time'] = pd.to_datetime(heartrate_seconds['Time'], format = "%m/%d/%Y %I:%M:%S %p")
@@ -64,18 +54,18 @@ def create_final_df(heartrate_seconds, daily_sleep):
     heartrate_daily['date_time'] = pd.to_datetime(heartrate_daily['date_time'], format = "%m/%d/%Y %I:%M:%S %p")
     heartrate_daily['date_time'] = heartrate_daily['date_time'].dt.date
     heartrate_daily['date_time'] = pd.to_datetime(heartrate_daily['date_time'], format = "%Y/%m/%d")
-    
+    heartrate_daily['day_of_week'] = heartrate_daily['date_time'].dt.day_name()
     daily_sleep['date_time'] = pd.to_datetime(daily_sleep['SleepDay'], format = '%m/%d/%Y %I:%M:%S %p')
     daily_values = heartrate_daily.merge(daily_sleep, how = 'left', on = ['Id', 'date_time'])
-    
+    daily_values['Sleep Duration'] = pd.cut(x = daily_values['TotalMinutesAsleep'], bins = [0, 393, 442, 503, 775], labels = ['Less', 'Okay', 'Enough', 'Healthy'])
     return daily_values
     
 if __name__ == '__main__':
     heartrate_seconds = pd.read_csv("../database/heartrate_seconds_merged.csv")
     daily_sleep = pd.read_csv("../database/sleepDay_merged.csv")
-    df_final_proc = create_final_df(heartrate_seconds, daily_sleep)
-    
-    
-    
-    
+    df_final_proc = create_final_df(heartrate_seconds, daily_sleep)   
+    plot_daily_heart_rate(df_final_proc, user_id = None)
+    plot_weekly_heart_rate(df_final_proc, user_id = None)
+    plot_bpm_density(df_final_proc, user_id = None)
+    plot_sleep_vs_bpm(df_final_proc, user_id = None)
     
