d95f68468ad94e468eec8dd631afc7cda32f8ad8
Akshit Miglani <akshit.miglani09@gmail.com>
2023-03-12 13:54:34 -0700

    Commit Activity and Weight Changes

--- app.py
+++ app.py
@@ -106,7 +106,15 @@ if selected_dropdown == 'Heart Rate':
     plt.show()
     
 if selected_dropdown == 'Activity & Weight':
-    st.write("Activity & Weight")
+    df_sleep_data_unproc = pd.read_csv("database/sleepDay_merged.csv")
+    df_daily_steps_unproc = pd.read_csv("database/dailySteps_merged.csv")
+    df_daily_calories_unproc = pd.read_csv("database/dailyCalories_merged.csv")
+    df_sleep_data_proc = plot_sleep_time_vs_time_in_bed(df_sleep_data_unproc, '1503960366')
+    df_daily_steps_proc = plot_daily_step_pattern(df_daily_steps_unproc, user_id=None)
+    df_sleep_and_steps_merged = pd.merge(df_daily_steps_proc, df_sleep_data_proc, how='inner',
+                                    left_on='ActivityDay', right_on='SleepDate')
+    plot_daily_sleep_vs_step_count(df_sleep_and_steps_merged)
+    df_daily_calories_proc = plot_daily_calories_pattern(df_daily_calories_unproc, user_id=None)
 
 if selected_dropdown == 'Caloric Model':
     dropdown_c_options = ['VeryActiveMinutes', 'LightlyActiveMinutes', 'SedentaryMinutes']
