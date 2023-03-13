dce2e5e072891e8b27056ea5afe39dec077e6e7d
Jeremie Poisson <jepoisso@microsoft.com>
2023-03-13 16:35:30 -0700

    fix uts

--- tests/test_calories_analysis.py
+++ tests/test_calories_analysis.py
@@ -12,15 +12,22 @@ import analysis
 class TestProcessDailyCaloriesData(unittest.TestCase):
 
     def setUp(self):
-        self.df_daily_calories = pd.DataFrame({
-            'Id': ['1503960366', '1503960366', '1927972279', '1927972279'],
+        self.df = pd.DataFrame({
+            'Id': [1503960366, 1503960366, 1927972279, 1927972279],
             'Calories': [1500, 2000, 1800, 1900]
         })
 
+    def test_process_daily_calories_data_smoke(self):
+        output = analysis.calories_analysis.process_daily_calories_data(self.df, '1503960366')
+        assert isinstance(output, pd.DataFrame)
+        assert len(output) == 2
+        assert set(output['Id']) == {1503960366}
+        assert set(output['Calories']) == {1500, 2000}
+
     def test_filter_df_user_id_called(self):
         with patch('analysis.calories_analysis.analysis_utils.filter_df_user_id') as mock_filter:
-            analysis.calories_analysis.process_daily_calories_data(self.df_daily_calories)
-            mock_filter.assert_called_once_with(self.df_daily_calories, '1503960366')
+            analysis.calories_analysis.process_daily_calories_data(self.df)
+            mock_filter.assert_called_once_with(self.df, '1503960366')
 
 if __name__ == "__main__":
     unittest.main()
