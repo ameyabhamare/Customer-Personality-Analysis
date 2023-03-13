1bf6a66f9057ed1d2e6d18517d6e02cac4d63755
Jeremie Poisson <jepoisso@microsoft.com>
2023-03-13 15:37:53 -0700

    docs

--- analysis/calories_analysis.py
+++ analysis/calories_analysis.py
@@ -3,5 +3,15 @@ from sklearn.linear_model import LinearRegression
 from utils import analysis_utils
 
 def process_daily_calories_data(df_daily_calories, user_id='1503960366'):
+    """
+    Filter daily calories data by user id and return the filtered dataframe.
+    
+    Args:
+        df_daily_calories (pandas.DataFrame): Dataframe containing daily calories data.
+        user_id (str): User id to filter the dataframe by.
+    
+    Returns:
+        pandas.DataFrame: Filtered dataframe.
+    """
     df_daily_calories = analysis_utils.filter_df_user_id(df_daily_calories, user_id)
     return df_daily_calories
--- analysis/heartrate_analysis.py
+++ analysis/heartrate_analysis.py
@@ -4,31 +4,35 @@ from utils import analysis_utils
 def process_heartrate_data(df_heartrate, df_daily_sleep, user_id = '2026352035'):
     """
     A method to process and merge the heart rate and sleep datasets
-    Arguments:
-        None
-    Returns:
-        A merged dataset containing heart rate data on a seconds level 
-        and sleep information on a daily level
+    :param df_heartrate: dataframe containing heart rate data
+    :param df_daily_sleep: dataframe containing sleep information
+    :param user_id: id of user to filter data for
+    :return: merged dataset containing heart rate data on a seconds level and sleep information on a daily level
     """
+    
+    # filter data by user_id
     df_heartrate = analysis_utils.filter_df_user_id(df_heartrate, user_id)
     df_daily_sleep = analysis_utils.filter_df_user_id(df_daily_sleep, user_id)
 
+    # convert date_time column to datetime format
     df_heartrate['date_time'] = pd.to_datetime(df_heartrate['Time'], format = "%m/%d/%Y %I:%M:%S %p")
 
-    # arrange heartrate daily
+    # arrange heart rate daily by taking mean of heart rate for each day
     heartrate_daily = df_heartrate.resample('1D', on = 'date_time', origin = '2016-04-12 07:21:00').Value.mean().reset_index()
 
+    # convert date_time column to date format
     heartrate_daily['date_time'] = pd.to_datetime(heartrate_daily['date_time'], format = "%m/%d/%Y %I:%M:%S %p")
     heartrate_daily['date_time'] = heartrate_daily['date_time'].dt.date
     heartrate_daily['date_time'] = pd.to_datetime(heartrate_daily['date_time'], format = "%Y/%m/%d")
     heartrate_daily['day_of_week'] = heartrate_daily['date_time'].dt.day_name()
 
-    # # arrange daily sleep
+    # convert SleepDay column to datetime format
     df_daily_sleep['date_time'] = pd.to_datetime(df_daily_sleep['SleepDay'], format = '%m/%d/%Y %I:%M:%S %p')
 
     # merge datasets
     daily_values = heartrate_daily.merge(df_daily_sleep, how = 'left', on = ['date_time'])
+
+    # categorize Sleep Duration column based on TotalMinutesAsleep
     daily_values['Sleep Duration'] = pd.cut(x = daily_values['TotalMinutesAsleep'], bins = [0, 393, 442, 503, 775], labels = ['Less', 'Okay', 'Enough', 'Healthy'])
 
     return daily_values
-    
