7b9efc30ce2d35a7df3365f8581279beb3b96132
ameyabhamare <ameyarb1804@gmail.com>
2023-03-12 17:21:18 -0700

    increasing pylint score

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+"""
+This module spins up a app on localhost using the streamlit library
+"""
+import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 import matplotlib
 matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
 
 st.title("FitMe")
 st.markdown("Fitness Explorer. This app performs health analysis based on fitness tracking data")
@@ -14,6 +17,12 @@ selected_dropdown = st.sidebar.selectbox("Select Analysis", options = dropdown_o
 
 # Add new features/columns to the dataframes
 def transform_dataframe(df):
+    """
+    Args:
+        df (_type_): raw pandas dataframe
+    Returns:
+        df: processed dataframe
+    """
     df['ActivityDate'] = pd.to_datetime(df['ActivityDate'])
     df['year'] = df['ActivityDate'].dt.year
     df['month'] = df['ActivityDate'].dt.month
@@ -23,6 +32,16 @@ def transform_dataframe(df):
 
 # This function is for the caloric model module
 def model_pipeline(df, x, y):
+    """_summary_
+
+    Args:
+        df (_type_): _description_
+        x (_type_): _description_
+        y (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     X = df.loc[:, x].values.reshape(-1, 1)  # values converts it into a numpy array
     Y = df.loc[:, 'Calories'].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
     linear_regressor = LinearRegression()  # create object for the class
@@ -36,9 +55,9 @@ for file_ in files:
     file_path = f'data/{file_name}'
     df = pd.read_csv(file_path)
     df = transform_dataframe(df)
-        
+    
 # Heart rate analysis
-if selected_dropdown == 'Heart Rate':   
+if selected_dropdown == 'Heart Rate':
     from heart_rate_analysis.heart_rate_analysis import create_final_df,\
     plot_daily_heart_rate, plot_weekly_heart_rate, plot_bpm_density, plot_sleep_vs_bpm
     daily_values = create_final_df()
@@ -46,25 +65,29 @@ if selected_dropdown == 'Heart Rate':
     plot_weekly_heart_rate(daily_values, user_id = None)
     plot_bpm_density(daily_values, user_id = None)
     plot_sleep_vs_bpm(daily_values, user_id = None)
-        
+# Activity and weight analysis
 if selected_dropdown == 'Activity & Weight':
-        from activity_and_weight_analysis.activity_and_weight_analysis import *
-        df_sleep_data_unproc = pd.read_csv("database/sleepDay_merged.csv")
-        df_daily_steps_unproc = pd.read_csv("database/dailySteps_merged.csv")
-        df_daily_calories_unproc = pd.read_csv("database/dailyCalories_merged.csv")
-        df_sleep_data_proc = plot_sleep_time_vs_time_in_bed(df_sleep_data_unproc, '1503960366')
-        df_daily_steps_proc = plot_daily_step_pattern(df_daily_steps_unproc, user_id=None)
-        df_sleep_and_steps_merged = pd.merge(df_daily_steps_proc, df_sleep_data_proc, how='inner',
-                                        left_on='ActivityDay', right_on='SleepDate')
-        plot_daily_sleep_vs_step_count(df_sleep_and_steps_merged)
-        df_daily_calories_proc = plot_daily_calories_pattern(df_daily_calories_unproc, user_id=None)
+    from activity_and_weight_analysis.activity_and_weight_analysis import plot_sleep_time_vs_time_in_bed,\
+    plot_daily_step_pattern, plot_daily_sleep_vs_step_count, plot_daily_calories_pattern
+    df_sleep_data_unproc = pd.read_csv("database/sleepDay_merged.csv")
+    df_daily_steps_unproc = pd.read_csv("database/dailySteps_merged.csv")
+    df_daily_calories_unproc = pd.read_csv("database/dailyCalories_merged.csv")
+    df_sleep_data_proc = plot_sleep_time_vs_time_in_bed(df_sleep_data_unproc, '1503960366')
+    df_daily_steps_proc = plot_daily_step_pattern(df_daily_steps_unproc, user_id=None)
+    df_sleep_and_steps_merged = pd.merge(df_daily_steps_proc, df_sleep_data_proc, how='inner',
+                                    left_on='ActivityDay', right_on='SleepDate')
+    plot_daily_sleep_vs_step_count(df_sleep_and_steps_merged)
+    df_daily_calories_proc = plot_daily_calories_pattern(df_daily_calories_unproc, user_id=None)
 
 if selected_dropdown == 'Caloric Model':
-    dropdown_c_options = ['VeryActiveMinutes', 'LightlyActiveMinutes', 'SedentaryMinutes', 'ModeratelyActiveDistance', 'VeryActiveDistance', 'SedentaryActiveDistance']
-    selected_c_dropdown = st.selectbox("Select Variable", options = dropdown_c_options)
+    dropdown_c_options = ['VeryActiveMinutes', 'LightlyActiveMinutes', 
+                          'SedentaryMinutes', 'ModeratelyActiveDistance', 'VeryActiveDistance', 'SedentaryActiveDistance']
+    selected_c_dropdown = st.selectbox("Select Variable", 
+                                       options = dropdown_c_options)
     slider_val = st.slider(selected_c_dropdown, min(df[selected_c_dropdown]), max(df[selected_c_dropdown]), 1)
     fig = plt.figure(figsize=(15, 8))
     sns.regplot(data = df, x= selected_c_dropdown, y = 'Calories')
     st.pyplot(fig)
     lr = model_pipeline(df, selected_c_dropdown, 'Calories')
     st.write(f"Model Daily Caloric: {round(lr.predict([[slider_val]])[0][0], 2)}")
+    
