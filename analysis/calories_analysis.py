aaba24d95f5ec8e18933488d8a1e4cff27ccf3eb
Jeremie Poisson <jepoisso@microsoft.com>
2023-03-13 14:54:06 -0700

    cleanups

--- analysis/calories_analysis.py
+++ analysis/calories_analysis.py
@@ -2,38 +2,6 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from utils import analysis_utils
 
-# def plot_daily_calories_pattern(df_daily_calories, user_id=None):
-#     """
-#     A method to generate a graph for daily calories.
-#     Args:
-#     df_sleep_data - Dataframe containing the number of calories burnt everyday.
-#     user_id (None: optional) - The user-id of the user.
-
-#     Return:
-#     df_sleep_data - The processed calories dataframe.
-#     """
-#     if user_id is None:
-#         user_id = '1503960366'
-#     df_daily_calories = df_daily_calories.query(f"Id == {user_id}")
-#     _, ax1 = plt.subplots(figsize=(12,6))
-#     sns.lineplot(data = df_daily_calories, x='ActivityDay', y='Calories', marker='o',
-#                     ax=ax1, color='g')
-#     plt.xticks(rotation=90)
-#     plt.show()
-#     return df_daily_calories
-
-# def transform_dataframe(df_):
-#     '''
-#     This function adds new features/columns to the dataframe
-#     '''
-#     df_['ActivityDate'] = pd.to_datetime(df_['ActivityDate'])
-#     df_['year'] = df_['ActivityDate'].dt.year
-#     df_['month'] = df_['ActivityDate'].dt.month
-#     df_['dayofweek'] = df_['ActivityDate'].dt.dayofweek
-#     
-#     df_['dayofweek'] = df_['dayofweek'].map(day_mapping)
-#     return df_
-
 def process_daily_calories_data(df_daily_calories, user_id='1503960366'):
     df_daily_calories = analysis_utils.filter_df_user_id(df_daily_calories, user_id)
     day_mapping = {0: 'Mon', 1: 'Tue', 2: 'Wed', 3: 'Thu', 4: 'Fri', 5: 'Sat', 6: 'Sun' }
