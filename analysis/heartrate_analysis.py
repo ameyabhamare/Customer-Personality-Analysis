a9445227a12a06361dbbca1c1edcb0eed6451c45
Jeremie Poisson <jepoisso@microsoft.com>
2023-03-13 18:02:09 -0700

    added heartrate analysis + uts and fixed a bunch of bugs

--- analysis/heartrate_analysis.py
+++ analysis/heartrate_analysis.py
@@ -18,19 +18,21 @@ def process_heartrate_data(df_heartrate, df_daily_sleep, user_id = '2026352035')
     df_heartrate['date_time'] = pd.to_datetime(df_heartrate['Time'], format = "%m/%d/%Y %I:%M:%S %p")
 
     # arrange heart rate daily by taking mean of heart rate for each day
-    heartrate_daily = df_heartrate.resample('1D', on = 'date_time', origin = '2016-04-12 07:21:00').Value.mean().reset_index()
+    heartrate_daily = df_heartrate.resample('1D', on = 'date_time', origin = '2016-04-12 00:00:00').Value.mean().reset_index()
 
     # convert date_time column to date format
-    heartrate_daily['date_time'] = pd.to_datetime(heartrate_daily['date_time'], format = "%m/%d/%Y %I:%M:%S %p")
-    heartrate_daily['date_time'] = heartrate_daily['date_time'].dt.date
     heartrate_daily['date_time'] = pd.to_datetime(heartrate_daily['date_time'], format = "%Y/%m/%d")
+    heartrate_daily['date'] = heartrate_daily['date_time'].dt.date
     heartrate_daily['day_of_week'] = heartrate_daily['date_time'].dt.day_name()
 
     # convert SleepDay column to datetime format
     df_daily_sleep['date_time'] = pd.to_datetime(df_daily_sleep['SleepDay'], format = '%m/%d/%Y %I:%M:%S %p')
+    df_daily_sleep['date'] = df_daily_sleep['date_time'].dt.date
 
     # merge datasets
-    daily_values = heartrate_daily.merge(df_daily_sleep, how = 'left', on = ['date_time'])
+    daily_values = heartrate_daily.merge(df_daily_sleep, how = 'left', on = ['date'])
+    daily_values = daily_values.rename(columns={"date_time_x": "date_time"})
+    daily_values = daily_values.drop(columns=['date_time_y'])
 
     # categorize Sleep Duration column based on TotalMinutesAsleep
     daily_values['Sleep Duration'] = pd.cut(x = daily_values['TotalMinutesAsleep'], bins = [0, 393, 442, 503, 775], labels = ['Less', 'Okay', 'Enough', 'Healthy'])
