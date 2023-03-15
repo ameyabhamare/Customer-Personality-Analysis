5050c493087faa0db5c401ffddfb0cca9810cd6f
Jeremie Poisson <jepoisso@microsoft.com>
2023-03-14 17:11:11 -0700

    added Uts for populate dropdown

--- tests/test_analysis_utils.py
+++ tests/test_analysis_utils.py
@@ -4,6 +4,7 @@ Unit tests for utils/analysis module
 import sys
 import os
 import unittest
+from unittest.mock import patch
 import pandas as pd
 
 
@@ -16,6 +17,7 @@ class TestAnalysisUtils(unittest.TestCase):
     """
     Test suite for analysis utils
     """
+
     def test_filter_df_user_id_valid_id(self):
         """
         Filters by user id
@@ -44,6 +46,23 @@ class TestAnalysisUtils(unittest.TestCase):
         # Act/Assert
         with self.assertRaises(TypeError):
             utils.analysis_utils.filter_df_user_id(df, user_id)
+
+    def test_populate_dropdowns(self):
+        """
+        Unit test for populate_dropdowns
+        """
+        with patch("utils.analysis_utils.pd") as pd_mock:
+            with patch("utils.analysis_utils.glob") as glob_mock:
+                pd_mock.read_csv.return_value = pd.DataFrame({
+                    'Id': [1, 2, 3]
+                })
+                glob_mock.glob.return_value = ["path1", "path2"]
+
+                store_ids = utils.analysis_utils.populate_dropdowns()
+
+                pd_mock.read_csv.assert_called()
+                glob_mock.glob.assert_called()
+                self.assertEqual(store_ids, set([1, 2, 3]))
 
 
 if __name__ == "__main__":
